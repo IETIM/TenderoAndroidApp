9b755a71b512784b78b962f26632efe53d413127
Andres Marcelo <felipemarcelo156@gmail.com>
2020-11-11 00:17:14 -0500

    Project Structure and Login view

---
+++ app/src/main/java/edu/eci/ieti/takeiteasysk/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.eci.ieti.takeiteasysk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void logIn(View view) {
+        // Do something in response to button
+    }
+}
