84dd23ee2c4388bc3308f09a05cf4d59db9b6db8
Jcro15 <45981880+Jcro15@users.noreply.github.com>
2020-11-12 14:56:04 -0500

    product form

---
+++ app/src/main/java/edu/eci/ieti/takeiteasysk/ui/NewProductForm.java
@@ -0,0 +1,14 @@
+package edu.eci.ieti.takeiteasysk.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewProductForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_product_form);
+    }
+}
